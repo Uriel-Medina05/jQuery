474a508ec84315e869c4c8e17345974609ca3270
Uriel Medina <umedina@unitedvirtualities.com>
2020-01-30 11:56:20 -0700

    jQuery UI

---
+++ jQuery-UI/exercises/widgets-i/scripts/page.js
@@ -0,0 +1,53 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+$(function(){
+
+	$("#dialog").dialog(
+		{
+			autoOpen: false,
+			modal: true,
+			resizable: false,
+			draggable: false,
+			position: "top",
+			buttons: {
+				"Yes, I do!": function(){
+					$('#dialog').dialog('close');
+					$('#dialogDecision').text("Yes I did!!");
+				 },
+				"No": function() { }
+			}
+
+		}
+	);
+
+
+	$('#openDialog').click(function() {
+		$('#dialog').dialog('open');
+	});
+
+	// $('#progress').progressbar({value: 60});
+
+	$('#progress').progressbar({value: 100});
+
+	var value = 100;
+
+	countdown();
+
+	function countdown(){
+		value--;
+		$('#progress').progressbar("option", "value", value);
+		
+
+		if (value > 0) {
+			setTimeout(countdown, 100);
+			$('#countdown').text(value);
+		}
+		else{
+			$('#countdown').text('Completed..!');
+			// $('#progress').hide();
+			$('#progress').disable();
+		}
+	}
+	
+});
